de9a728ff6aad6c36f74374efbfa9d17b105c266
yesuraju84155 <yesuraju8415@gmail.com>
2024-10-16 15:18:27 +0530

    Update server.js
    cron import is commented

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import postRoutes from "./routes/postRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import { v2 as cloudinary } from "cloudinary";
 import { app, server } from "./socket/socket.js";
-import job from "./cron/cron.js";
+//import job from "./cron/cron.js";
 
 dotenv.config();
 
